d50109d5fc8b93e38a7887d8ada6b22df803aaa0
chie8842 <chie-hayashida@cookpad.com>
2019-05-18 18:48:40 +0900

    Makefile to bin files

--- proofreading.sh
+++ proofreading.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
 
+# convert one ipynb to markdown and save to output directory
 function create_markdown() {
+  dir=`dirname ${file}`
+  output_dir=${dir//site/proofreading\/output}
+  echo $output_dir
+  mkdir -p ${output_dir}
+  jupyter nbconvert --to markdown ${file} --output-dir ${output_dir}
+}
+
+# find ipynb files, convert to markdown and save to output directory
+function create_markdowns() {
   files=`find site/ja -maxdepth 5 -type f |grep .ipynb`
   for file in ${files}; do
-    dir=`dirname ${file}`
-    output_dir=${dir//site\/ja/proofreading\/output\/ja}
-    echo $output_dir
-    mkdir -p ${output_dir}
-    jupyter nbconvert --to markdown ${file} --output-dir ${output_dir}
+    create_markdown
   done
 }
 
+# execute redpen check to markdown files in output directory
 function exec_redpen() {
-  docs=`find proofreading/output/ja -maxdepth 3 -type f |grep .md`
+  docs=`find proofreading/output/${lang} -maxdepth 3 -type f |grep .md`
   redpen --conf proofreading/redpen-conf.xml ${docs}
 }
 
 lang=ja
-create_markdown
-exec_redpen
+
+# 引数の数が1つあったらその引数で与えられたファイルのみチェックする
+if [ $# -eq 1 ]; then
+  file=$1
+  echo "check 1 file: ${file}"
+  if [ ${file##*.} = "ipynb" ]; then
+    create_markdown
+    base_filename=${file##*/}
+    redpen --conf proofreading/redpen-conf.xml ${output_dir}/${base_filename%.*}.md
+  elif [ ${file##*.} = "md" ]; then
+    redpen --conf proofreading/redpen-conf.xml ${file}
+  else
+    echo "invalid file type"
+    exit 1
+  fi
+# 引数が1つ以外だったら全ファイルチェックする
+else
+  echo "check all files"
+  create_markdowns
+  exec_redpen
+fi
